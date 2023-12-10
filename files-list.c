428935f49fcb95193c524e704c0af7130340d605
TalhaK28 <153178599+TalhaK28@users.noreply.github.com>
2023-12-10 20:19:22 +0100

    Update files-list.c

--- files-list.c
+++ files-list.c
@@ -28,6 +28,42 @@ void clear_files_list(files_list_t *list) {
  *  @return a pointer to the added element if success, NULL else (out of memory)
  */
 files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
+
+        // Vérifier si le fichier existe déjà dans la liste
+    files_list_entry_t *existing_entry = find_entry_by_name(list, file_path, 0, 0);
+    if (existing_entry != NULL) {
+        return NULL;  // Le fichier existe déjà, ne rien faire
+    }
+
+    // Créer une nouvelle entrée pour le fichier
+    files_list_entry_t *new_entry = (files_list_entry_t *)malloc(sizeof(files_list_entry_t));
+    if (new_entry == NULL) {
+        return NULL;  // Échec d'allocation mémoire
+    }
+
+    // Remplir les propriétés de la nouvelle entrée en utilisant stat sur le fichier
+    struct stat file_stat;
+    if (stat(file_path, &file_stat) != 0) {
+        free(new_entry);
+        return NULL;  // Échec de l'obtention des informations sur le fichier
+    }
+
+    strncpy(new_entry->path_and_name, file_path, sizeof(new_entry->path_and_name));
+    new_entry->path_and_name[sizeof(new_entry->path_and_name) - 1] = '\0';
+    new_entry->mtime.tv_sec = file_stat.st_mtime;
+    new_entry->mtime.tv_nsec = 0;
+    new_entry->size = file_stat.st_size;
+    new_entry->entry_type = S_ISDIR(file_stat.st_mode) ? DOSSIER : FICHIER;
+    new_entry->mode = file_stat.st_mode;
+    memset(new_entry->md5sum, 0, sizeof(new_entry->md5sum));  // Remplir le MD5 à votre discrétion
+
+    // Ajouter l'entrée à la liste
+    if (add_entry_to_tail(list, new_entry) != 0) {
+        free(new_entry);
+        return NULL;  // Échec d'ajout à la liste
+    }
+
+    return new_entry;    
 }
 
 /*!
@@ -39,6 +75,26 @@ files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
  * @return 0 in case of success, -1 else
  */
 int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
+
+        if (list == NULL || entry == NULL) {
+        return -1;  // Paramètres invalides
+    }
+
+    // Ajouter l'entrée à la fin de la liste
+    entry->next = NULL;
+    entry->prev = list->tail;
+
+    if (list->tail == NULL) {
+        // La liste est vide, donc l'élément ajouté devient la tête et la queue
+        list->head = entry;
+        list->tail = entry;
+    } else {
+        // Ajouter à la fin de la liste
+        list->tail->next = entry;
+        list->tail = entry;
+    }
+
+    return 0;  // Succès
 }
 
 /*!
@@ -51,6 +107,20 @@ int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
  *  @return a pointer to the element found, NULL if none were found.
  */
 files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size_t start_of_src, size_t start_of_dest) {
+
+        if (list == NULL || file_path == NULL) {
+        return NULL;  // Paramètres invalides
+    }
+
+    // Parcourir la liste pour trouver l'entrée correspondante
+    for (files_list_entry_t *cursor = list->head; cursor != NULL; cursor = cursor->next) {
+        // Comparer les noms des fichiers en ignorant les parties de chemin spécifiées
+        if (strcmp(cursor->path_and_name + start_of_src, file_path + start_of_dest) == 0) {
+            return cursor;  // Entrée trouvée
+        }
+    }
+
+    return NULL;  // Aucune entrée trouvée
 }
 
 /*!
