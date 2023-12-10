913230053d7dbb3013930f90ab10355246d280de
SamuelMolieres <153177705+SamuelMolieres@users.noreply.github.com>
2023-12-10 16:30:38 +0100

    Add files via upload

---
+++ configuration.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    char source[1024];
+    char destination[1024];
+    uint8_t processes_count;
+    bool is_parallel;
+    bool uses_md5;
+		bool is_verbose;
+		bool is_dry_run;
+} configuration_t;
+
+void init_configuration(configuration_t *the_config);
+int set_configuration(configuration_t *the_config, int argc, char *argv[]);
